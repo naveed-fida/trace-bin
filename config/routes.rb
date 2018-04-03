3e51e2b1ae385dc9ed983fd28a502fc1d7755238
Naveed Fida <naveedfida01@gmail.com>
2018-04-03 05:25:05 +0500

    only story route

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'app_bins#new'
+  root to: 'pages#story'
   get 'story', to: 'pages#story'
-
-  resources :reports, only: [:create]
-  resources :app_bins, only: [:show, :create] do
-    resources :endpoints, only: [:index, :show]
-    resources :background_jobs, only: [:index]
-
-    resource :traffic_metrics, only: [:show]
-    resource :memory_metrics, only: [:show]
-    resource :cpu_metrics, only: [:show]
-  end
 end
